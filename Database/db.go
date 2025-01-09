51a038bd68a9d46a6ff53c4ec50a52ec75ddb690
Nikhil <nikhilayush2020@gmail.com>
2025-01-10 04:12:37 +0530

    batman3

--- Database/db.go
+++ Database/db.go
@@ -1,48 +1,89 @@
 package database
 
+import (
+	"errors"
+	"fmt"
+)
+
 type User struct {
 	Username string `json:"username"`
 	Email    string `json:"email"`
 	Password string `json:"password"`
-	PhoneNo  string `json:"phone-no"`
-	NextUser *User  `json:"next_user"`
-	PrevUser *User  `json:"prev_user"`
+	PhoneNo  string `json:"phone_no"`
+	NextUser *User  `json:"-"`
+	PrevUser *User  `json:"-"`
 }
 
-// Constructor function for User struct
-// returns a pointer to a User struct
-func NewUser(username, email, password, phoneNo string) *User {
-	return &User{
+type LinkedList struct {
+	Head *User
+}
+
+func (list *LinkedList) NewUser(username, email, password, phoneNo string) *User {
+	newUser := &User{
 		Username: username,
 		Email:    email,
 		Password: password,
 		PhoneNo:  phoneNo,
-		NextUser: nil,
-		PrevUser: nil,
+	}
+
+	if list.Head == nil {
+		list.Head = newUser
+		return newUser
+	}
+
+	current := list.Head
+	for current.NextUser != nil {
+		current = current.NextUser
+	}
+	current.NextUser = newUser
+	newUser.PrevUser = current
+
+	return newUser
+}
+
+// FindUser function searches for a user by username and returns the user pointer if present or nil if not present
+func (list *LinkedList) FindUser(username string) (*User, error) {
+	current := list.Head
+	for current != nil {
+		if current.Username == username {
+			return current, nil
+		}
+		current = current.NextUser
+	}
+	return nil, errors.New("User not found")
+}
+
+// DeleteUser function deletes a user by username
+func (list *LinkedList) DeleteUser(username string) {
+	if list.Head == nil {
+		return
+	}
+	if list.Head.Username == username {
+		list.Head = list.Head.NextUser
+		if list.Head != nil {
+			list.Head.PrevUser = nil
+		}
+		return
+	}
+	current := list.Head
+	for current.NextUser != nil {
+		if current.NextUser.Username == username {
+			current.NextUser = current.NextUser.NextUser
+			if current.NextUser != nil {
+				current.NextUser.PrevUser = current
+			}
+			return
+		}
+		current = current.NextUser
 	}
 }
 
-// let us imagine an head node i.e a pointer demo user
-func DeleteUser(Head *User, username string) *User {
-	// If the head node itself holds the username to be deleted
-	if Head != nil && Head.Username == username {
-		return Head.NextUser
+// Traverse function to print all users in the linked list
+// DEBUGGING PURPOSES
+func (list *LinkedList) Traverse() {
+	current := list.Head
+	for current != nil {
+		fmt.Printf("Username: %s, Email: %s, PhoneNo: %s\n", current.Username, current.Email, current.PhoneNo)
+		current = current.NextUser
 	}
-
-	// Search for the username to be deleted, keep track of the previous node
-	// as we need to change 'prev.Next'
-	prev := Head
-	for prev.NextUser != nil && prev.NextUser.Username != username {
-		prev = prev.NextUser
-	}
-
-	// If the username was not present in linked list
-	if prev.NextUser == nil {
-		return Head
-	}
-
-	// Unlink the node from linked list
-	prev.NextUser = prev.NextUser.NextUser
-
-	return Head
 }
--- main.go
+++ main.go
@@ -10,13 +10,14 @@ import (
 
 func main() {
 	r := mux.NewRouter()
+	fs := http.FileServer(http.Dir("./static"))
+	r.PathPrefix("/static/").Handler(http.StripPrefix("/static/", fs))
 	r.HandleFunc("/Login", routes.Login)
 	r.HandleFunc("/", serveHome)
 	r.HandleFunc("/Signup", routes.Signup)
-	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("helper"))))
 
-	println("Server started at http://localhost:6969/")
-	log.Fatal(http.ListenAndServe(":6969", r))
+	println("Server started at http://localhost:8000/")
+	log.Fatal(http.ListenAndServe(":8000", r))
 
 }
 func serveHome(w http.ResponseWriter, r *http.Request) {
